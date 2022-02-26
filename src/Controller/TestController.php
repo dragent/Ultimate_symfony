ec2d96b2053e115536b5778ab3650f75c2adf6bc
Alexis CAU <dragent@users.noreply.github.com>
2022-02-26 15:45:09 +0100

    Premiere parties annotations

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,26 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class TestController extends AbstractController
 {
+    /**
+     * @Route("/", name="index")
+     */
     public function index(): Response
     {
         dd("Ca fonctionne");
     }
 
     /**
-     * @Route(
-     *  "/test/{age<\d+>?0", 
-     *  name="app_test", 
-     *  methods={"GET","POST"},
-     *  host="localhost",
-     *  schemes={"http","https"}
-     * )
+     * @Route("/test/{age<\d+>?0}", name="test" , methods={"GET","POST"},  host="localhost",  schemes={"http","https"})
      */
     public function test(Request $request, $age)
     {
