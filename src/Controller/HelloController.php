06de3b1cabf5f6091664c0d6cbbb02e2122da785
Alexis CAU <dragent@users.noreply.github.com>
2022-02-26 23:03:18 +0100

    decouverte notion container

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HelloController
+{
+    private LoggerInterface $logger;
+    private Calculator $calculator;
+
+    public function __construct(LoggerInterface $logger, Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @Route ("/hello/{firstname?World}", name="hello_name", methods={"GET"})
+     */
+    public function hello($firstname, Slugify $slugify)
+    {
+        dump($slugify->slugify("Hello world"));
+        $this->logger->info("mon message de log");
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
+        return new Response("Hello $firstname");
+    }
+}
