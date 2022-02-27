f8a05059230fdd0083cc3c6ee3a3b7bd3d0a59ee
Alexis CAU <dragent@users.noreply.github.com>
2022-02-27 23:23:47 +0100

    Milieu twig chapitre 1

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,36 +2,33 @@
 
 namespace App\Controller;
 
-use App\Taxes\Calculator;
-use App\Taxes\Detector;
-use Cocur\Slugify\Slugify;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Twig\Environment;
 
 class HelloController
 {
-    private LoggerInterface $logger;
-    private Calculator $calculator;
 
-    public function __construct(LoggerInterface $logger, Calculator $calculator)
-    {
-        $this->calculator = $calculator;
-        $this->logger = $logger;
-    }
+
 
     /**
      * @Route ("/hello/{firstname?World}", name="hello_name", methods={"GET"})
      */
-    public function hello($firstname, Slugify $slugify, Detector $detector)
+    public function hello($firstname, Environment $twig)
     {
-        dump($detector->detect(101));
-        dump($detector->detect(10));
-        dump($slugify->slugify("Hello world"));
-        $this->logger->info("mon message de log");
-        $tva = $this->calculator->calcul(100);
-        dump($tva);
-        return new Response("Hello $firstname");
+        $html = $twig->render("hello.html.twig", [
+            "prenom" => $firstname,
+            "formateur1" =>  [
+                "prenom" => "lior",
+                "nom" => "chamla",
+                "age" => 33
+            ],
+            "formateur2" =>  [
+                "prenom" => "denise",
+                "nom" => "test",
+                "age" => 3
+            ],
+        ]);
+        return new Response($html);
     }
 }
