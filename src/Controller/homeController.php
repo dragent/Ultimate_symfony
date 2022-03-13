ea15edc465bdd9bba3c4d4f07c2c4a82c3d68636
Alexis CAU <dragent@users.noreply.github.com>
2022-03-13 22:59:03 +0100

    fin de doctrine et installation de nos premieres entites

--- src/Controller/homeController.php
+++ src/Controller/homeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,8 +13,11 @@ class homeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage()
+    public function homepage(ProductRepository $productRepository)
     {
-        return $this->render("home/homepage.html.twig");
+        $products = $productRepository->findBy([], [], 3);
+        return $this->render("home/homepage.html.twig", [
+            "products" => $products
+        ]);
     }
 }
